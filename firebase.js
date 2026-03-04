c8af43d7cf304f585072d56ab489d867c1911f46
S-T-A-S-I-S <montana.morgan@iavalley.edu>
2026-03-04 13:26:01 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,8 @@
 // firebase.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
-// Your Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAunNnSwaIdSzq4t1jebGCdqJaTEtcQoFbk",
   authDomain: "kart-kapitol-89d82.firebaseapp.com",
@@ -14,10 +12,8 @@ const firebaseConfig = {
   appId: "1:488402375742:web:012cbddd266b07f4ba2d6f"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
