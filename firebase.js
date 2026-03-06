3b5415e21d0f96a9cdcce25dbebaaf8ef8d103de
S-T-A-S-I-S <montana.morgan@iavalley.edu>
2026-03-05 22:54:15 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,18 @@
 // firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAunNnSwaIdSzq4t1jebGCdqJaTEtcQoFbk",
+  apiKey: "AIzaSyBRbb4HehSRvxHuZY5FF1Gy9K4z1m5BeKs",
   authDomain: "kart-kapitol-89d82.firebaseapp.com",
   projectId: "kart-kapitol-89d82",
   storageBucket: "kart-kapitol-89d82.firebasestorage.app",
   messagingSenderId: "488402375742",
-  appId: "1:488402375742:web:012cbddd266b07f4ba2d6f"
+  appId: "1:488402375742:web:e5ecafa834857f25ba2d6f"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const googleProvider = new GoogleAuthProvider();
